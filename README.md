b13968d83c7fdd49fcb2d283a6542540cc49c598
root <root@DESKTOP-E55I23J.localdomain>
2021-12-29 23:16:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,97 @@
+<h1 align="center">
+	<!--
+    Here you can put a logo, the following formatt is recommended:
+    <img alt="Logo" src=".github/logo.png" width="200px" /> 
+  -->
+</h1>
+
+<h3 align="center">
+  Certificate Generator
+</h3>
+
+<p align="center">
+  <a href="#-about-the-project">About the project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-technologies">Getting started</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-how-to-contribute">How to contribute</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-license">License</a>
+</p>
+
+## 💻 About the project
+
+- <p style="color: red;">Basically it is a Certificate Generator. In which user upload csv and template of certificate and then select the area on the template where here he/she want to add data like Name , Position etc and after that we will give them the certificates and there are two option to download the certificate
+- We can download them by downloading each certificate 
+ 
+ <p style="center">OR</p>
+
+- We can download them by just clicking download all and we  got zip file in which we have our certificates
+</p>
+
+## 🚀 Technologies
+
+- NodeJS
+- Ejs
+- Materialize
+- jimp
+- JCrop
+
+## 💻 Getting started
+
+-Just clone the project start the server and enjoy
+-Quick Demo at https://rocky-coast-52940.herokuapp.com/<!-- Here you can put how to setup your application-->
+
+### Requirements
+
+- Null<!-- put each requirement here -->
+
+**Clone the project and access the folder**
+
+<!-- you can put the commands inside the three grave accents -->
+
+```bash
+$ git clone https://github.com/Greatgabbar/vithack.git
+$ cd vithack
+```
+
+**Follow the steps below**
+
+<!-- you can put the commands inside the three grave accents -->
+
+```bash
+npm i
+node app.js
+```
+
+## 🤔 How to contribute
+
+**Make a fork of this repository**
+
+```bash
+# Fork using GitHub official command line
+# If you don't have the GitHub CLI, use the web site to do that.
+
+$ gh repo fork EliasGcf/NOME_DO_REPO
+```
+
+**Follow the steps below**
+
+```bash
+# Clone your fork
+$ git clone your-fork-url && cd NOME_DO_REPO
+
+# Create a branch with your feature
+$ git checkout -b my-feature
+
+# Make the commit with your changes
+$ git commit -m 'feat: My new feature'
+
+# Send the code to your remote branch
+$ git push origin my-feature
+```
+
+After your pull request is merged, you can delete your branch
+
+## 📝 License
+
+<!-- You can delete the license if you don't want it -->
+
---
+++ app.js
@@ -0,0 +1,158 @@
+const express = require('express');
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
+const Jimp = require('jimp');
+const bodyParser = require('body-parser');
+const csv = require('csvtojson');
+const fs=require('fs');
+const { createDecipher } = require('crypto');
+const admZip=require('adm-zip');
+const { Z_PARTIAL_FLUSH } = require('zlib');
+
+// Init app
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).array('myImage', 2);
+
+// Check File Type
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|csv|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+
+// EJS
+app.set('view engine', 'ejs');
+
+// Public Folder
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.render('index', {
+        msg: err
+      });
+    }
+    else {
+      if (req.files == undefined) {
+        res.render('index', {
+          msg: 'Error: No File Selected!'
+        });
+      } else {
+        res.render('edit', {
+          file: `uploads/${req.files[1].filename}`,
+          csv: `uploads/${req.files[0].filename}`,
+        });
+      }
+    }
+  });
+});
+
+app.post('/api/coordinates', (req, res) => {
+  csv().fromFile(`./public/${req.body['3'].csv}`).then((data) => {
+    const name= req.body['3'].file.split('.').slice(0,-1).join('.');
+    let count=0;
+    data.forEach((cred,i) => {
+      count++;
+      Jimp.read(`./public/${req.body[3].file}`)
+        .then(image => {
+          Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
+            // console.log(req.body);
+            image.print(font, req.body['0'].x, req.body['0'].y + 10, { text: cred.Name }, req.body[0].w);
+            image.print(font, req.body['1'].x, req.body['1'].y, { text: cred.Position }, req.body[1].w);
+            image.print(font, req.body[2].x, req.body[2].y, { text: cred.project }, req.body[2].w);
+            image.write(`./public/final/${name}/${i}.png`);
+          });
+        })
+    })
+    res.status(200).json({
+      file : name,
+      len : count
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+})
+
+app.get('/final', (req, res) => {
+  res.render('final');
+})
+
+app.get('/api/all/:name',(req,res)=>{
+  const filename=req.params.name;
+  // console.log(filename)
+  const zip=new admZip();
+  const upload=fs.readdirSync(`./public/final/uploads/${filename}`);
+  for(let i=0;i < upload.length; i++){
+    zip.addLocalFile(`./public/final/uploads/${filename}/${upload[i]}`);
+  }
+
+  const data=zip.toBuffer();
+  // zip.writeZip('./public/final-zip');
+
+  res.set('Content-Type','application/octet-stream');
+  res.set('Content-Disposition',`attachment; filename=\`GG.zip\``);
+  res.set('Content-Length',data.length);
+  res.send(data);
+
+})
+
+String.prototype.replaceAt = function(index, replacement) {
+  return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
+app.get('/api/:name',(req,res)=>{
+  let ename=req.params.name;
+  const file=ename.lastIndexOf('-'); 
+  const filename=ename.replaceAt(file,'/')
+  console.log(filename)
+  res.download(__dirname + `/public/final/uploads/${filename}`, 'Download.png', (err) => {
+    if (err) {
+      res.status(500).send({
+        message: "Could not download the file. " + err,
+      });
+    }
+  });
+})
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
